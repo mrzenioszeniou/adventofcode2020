e6d42b5a8b26563b56a4f8479383d1417d6abb47
zzeniou <zzeniou@apple.com>
2020-12-12 16:23:58 +0100

    Manhattan rotation for part 1

--- src/day12.rs
+++ src/day12.rs
@@ -1,5 +1,4 @@
 use crate::parse;
-use std::f64::consts::PI;
 
 pub fn solve() {
   let instructions = parse::parse_day12("input/12.txt");
@@ -10,18 +9,18 @@ pub fn solve() {
 fn part1(instructions: &Vec<(char, isize)>) {
   let mut x: isize = 0;
   let mut y: isize = 0;
-  let mut theta: f64 = 0_f64;
+  let mut angle: (isize, isize) = (1, 0);
   for instruction in instructions.iter() {
     match instruction {
       ('N', n) => y += n,
       ('S', n) => y -= n,
       ('E', n) => x += n,
       ('W', n) => x -= n,
-      ('L', n) => theta -= *n as f64 * PI / 180_f64,
-      ('R', n) => theta += *n as f64 * PI / 180_f64,
+      ('L', n) => rotate_left(&mut angle.0, &mut angle.1, *n),
+      ('R', n) => rotate_right(&mut angle.0, &mut angle.1, *n),
       ('F', n) => {
-        x += n * theta.cos() as isize;
-        y += -n * theta.sin() as isize;
+        x += n * angle.0;
+        y += n * angle.1;
       }
       (action, _) => panic!("Invalid action {}", action),
     }
