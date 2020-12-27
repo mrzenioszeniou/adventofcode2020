8d6cd45571e8fe91b4430577d63270f5c0b900d2
Zenios <19194653+trypes@users.noreply.github.com>
2020-12-27 22:11:01 +0100

    Use vector as a circular linked list

--- src/day23.rs
+++ src/day23.rs
@@ -7,80 +7,65 @@ pub fn solve() {
 }
 
 /// Solves Part 1
-fn part_1(mut cups: Vec<usize>) -> String {
-  rounds(&mut cups, 9, 100);
-
-  let start = cups.iter().position(|&e| e == 1).unwrap();
+fn part_1(cups: Vec<usize>) -> String {
+  let next = rounds(cups, 9, 100);
 
   let mut ans = String::new();
 
-  for i in 1..9 {
-    write!(ans, "{}", cups[(start + i) % cups.len()]).unwrap();
+  let mut curr = 1;
+  for _ in 0..8 {
+    write!(ans, "{}", next[curr]).unwrap();
+    curr = next[curr];
   }
 
   ans
 }
 
 /// Solves Part 2
-fn part_2(mut cups: Vec<usize>) -> usize {
-  rounds(&mut cups, 1_000_000, 10_000_000);
+fn part_2(cups: Vec<usize>) -> usize {
+  let next = rounds(cups, 1_000_000, 10_000_000);
 
-  let start = cups.iter().position(|&e| e == 1).unwrap();
-
-  for cup in cups.iter() {
-    println!("{}", cup);
-  }
-
-  cups[(start + 1) % cups.len()] * cups[(start + 2) % cups.len()]
+  next[1] * next[next[1]]
 }
 
 /// Performs a number of game rounds on the provided arrangement of cups
-fn rounds(cups: &mut Vec<usize>, lim: usize, rounds: usize) {
+fn rounds(mut cups: Vec<usize>, lim: usize, rounds: usize) -> Vec<usize> {
   let m = cups.iter().max().unwrap().clone();
 
   cups.append(&mut (m + 1..=lim).collect());
   assert_eq!(lim, cups.len());
 
-  let mut current_idx = 0;
+  let mut next = vec![0; cups.len() + 1];
+  for i in 0..cups.len() {
+    next[cups[i]] = cups[(i + 1) % cups.len()];
+  }
+
+  let mut current = cups[0];
   let bar = ProgressBar::new(rounds as u64);
   bar.set_style(
     ProgressStyle::default_bar()
-      .template(
-        "[{percent:>3}%] {bar:40.cyan/blue} {elapsed:>3} elapsed | ETA:{eta_precise} remaining",
-      )
+      .template("[{percent:>3}%] {bar:40.cyan/blue} {elapsed:>3} elapsed | {eta:>3} remaining")
       .progress_chars("##-"),
   );
   for _ in 0..rounds {
-    current_idx = round(cups, current_idx);
+    current = round(&mut next, current);
     bar.inc(1);
   }
 
   bar.finish();
+
+  next
 }
 
 /// Performs one game round on the provided arrangement of cups
-fn round(cups: &mut Vec<usize>, current_idx: usize) -> usize {
-  // print!("Cups: ");
-  // for (i, cup) in cups.iter().enumerate() {
-  //   if i == current_idx {
-  //     print!("({}) ", cup);
-  //   } else {
-  //     print!(" {}  ", cup);
-  //   }
-  // }
-  let max = cups.len();
-  let current = cups[current_idx].clone();
-  let mut pickups = vec![];
-  let mut curr_shift = 0;
-  for _ in 0..3 {
-    if current_idx + 1 < cups.len() {
-      pickups.push(cups.remove(current_idx + 1));
-    } else {
-      pickups.push(cups.remove(0));
-      curr_shift += 1;
-    }
-  }
-  // println!("Pickups: {:?}", pickups);
+fn round(next: &mut Vec<usize>, current: usize) -> usize {
+  let max = next.len() - 1;
+
+  let pickups = vec![
+    next[current],
+    next[next[current]],
+    next[next[next[current]]],
+  ];
 
   let mut destination = current - 1;
   loop {
@@ -93,16 +78,11 @@ fn round(cups: &mut Vec<usize>, current_idx: usize) -> usize {
     }
   }
 
-  let destination_idx = cups.iter().position(|&e| e == destination).unwrap();
-  cups.insert(destination_idx + 1, pickups.pop().unwrap());
-  cups.insert(destination_idx + 1, pickups.pop().unwrap());
-  cups.insert(destination_idx + 1, pickups.pop().unwrap());
+  next[current] = next[pickups[2]];
+  next[pickups[2]] = next[destination];
+  next[destination] = pickups[0];
 
-  if destination_idx > current_idx {
-    current_idx + 1
-  } else {
-    (current_idx + 4 - curr_shift) % max
-  }
+  next[current]
 }
 
 #[test]
