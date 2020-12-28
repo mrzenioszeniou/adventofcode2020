f1187c118ac92c8219454f94a29396c9334d25b1
Zenios <19194653+trypes@users.noreply.github.com>
2020-12-28 19:07:23 +0100

    Small fix

--- src/day25.rs
+++ src/day25.rs
@@ -4,7 +4,7 @@ pub fn solve() {
 }
 
 fn part_1(card_pk: usize, door_pk: usize) -> usize {
-  let mut card_loop_cnt = 0;
+  let mut card_loop_cnt = 1;
   let mut value = transform(7, 1, None);
   while value != card_pk {
     value = transform(7, 1, Some(value));
@@ -33,6 +33,6 @@ mod test {
   fn test() {
     assert_eq!(transform(7, 8, None), 5764801);
     assert_eq!(transform(7, 11, None), 17807724);
-    assert_eq!(part_1(5764801, 17807724, N))
+    assert_eq!(part_1(5764801, 17807724), 14897079);
   }
 }
